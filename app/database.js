201d55067770092221c824efc4b898a2a8f029f2
versus33 <versus333@gmail.com>
2022-01-18 01:14:22 +0100

    Delete app directory

--- app/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/sociosMongodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(db => console.log('Base de datos Conectada'))
-  .catch(err => console.log(err));
